25124a799d8b0161ef7081510d89df10ce28c763
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-31 00:29:24 +0530

    [Ravina] Added: Custom Exception and defining all the exceptions

--- DAY_21_UserRegistrationExceptionHandling/ExceptionHandling.cs
+++ DAY_21_UserRegistrationExceptionHandling/ExceptionHandling.cs
@@ -6,20 +6,15 @@ namespace UserRegistrationExceptionHandling
 {
     public class ExceptionHandling : Exception
     {
-
         public enum ExceptionType
         {
             NULL_FIELD,
-          
             INVALID_INPUT,
         }
         private readonly ExceptionType type;
         public ExceptionHandling(ExceptionType Type, string message) : base(message)
         {
             this.type = Type;
-
         }
-
     }
-
 }
