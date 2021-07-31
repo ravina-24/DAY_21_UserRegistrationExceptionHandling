679db0f4020005e643ded4d06324174320b77aa9
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-31 13:00:33 +0530

    [Ravina] Added : Checking Pattern for last name and Adding the exceptions

--- DAY_21_UserRegistrationExceptionHandling/Pattern.cs
+++ DAY_21_UserRegistrationExceptionHandling/Pattern.cs
@@ -7,6 +7,7 @@ namespace UserRegistrationExceptionHandling
     public class Pattern
     {
         public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
         public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
@@ -15,20 +16,46 @@ namespace UserRegistrationExceptionHandling
         public static void checkFirstName(string FirstName)
         {
             try
-            { 
+            {
                 if (FirstName == String.Empty || FirstName == " ")
                 {
-                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD,"Field is empty ");
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD, "Field is empty ");
                 }
-                else  if (Regex.IsMatch(FirstName, Regex_FirstName) == false )
+                else if (Regex.IsMatch(FirstName, Regex_FirstName) == false)
                 {
-                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,"Invalid input please check the first letter ");
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,
+                        "Invalid FirstName please check the first letter ");
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Field  ={0}", e.Message);
+                Console.WriteLine("Field exception ={0}", e.Message);
             }
+        }
+        public bool ValidateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_LastName);
+
+        }
+        public static void checkLastName(string LastName)
+        {
+            try
+            {
+                if (LastName == String.Empty || LastName == " ")
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD, "Field is empty ");
+                }
+                else if (Regex.IsMatch(LastName, Regex_FirstName) == false)
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,
+                        "Invalid LastName please check the first letter ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Field exception ={0}", e.Message);
+            }
+
         }
     }
 }
