76f8e09d9962c6efd0a968c04eae46fe27de069f
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-31 13:13:23 +0530

    [Ravina] Added : Checking pattern for email Adddress and Adding exceptions

--- DAY_21_UserRegistrationExceptionHandling/Pattern.cs
+++ DAY_21_UserRegistrationExceptionHandling/Pattern.cs
@@ -8,6 +8,7 @@ namespace UserRegistrationExceptionHandling
     {
         public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
         public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
+        public static string Regex_EmailId = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
@@ -56,6 +57,31 @@ namespace UserRegistrationExceptionHandling
                 Console.WriteLine("Field exception ={0}", e.Message);
             }
 
+
+        }
+        public bool ValidateEmailId(string EmailId)
+        {
+            return Regex.IsMatch(EmailId, Regex_EmailId);
+
+        }
+        public static void CheckEmail(string EmailId)
+        {
+            try
+            {
+                if (EmailId == String.Empty || EmailId == " ")
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.NULL_FIELD, "Field is empty ");
+                }
+                else if (Regex.IsMatch(EmailId, Regex_FirstName) == false)
+                {
+                    throw new ExceptionHandling(ExceptionHandling.ExceptionType.INVALID_INPUT,
+                        "Invalid EmailId");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Field exception ={0}", e.Message);
+            }
         }
     }
 }
